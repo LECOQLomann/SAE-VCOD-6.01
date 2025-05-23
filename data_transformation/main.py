e24ab4aa8d7648549fc8bba10601d9099152d796
LECOQ Lomann <lomannlcq@hotmail.fr>
2025-05-23 15:55:28 +0200

    up transformation

--- data_transformation/main.py
+++ data_transformation/main.py
@@ -3,62 +3,72 @@ import os
 import json
 from datetime import datetime
 
-postgres_db=os.environ.get('POSTGRES_DB')
-postgres_user=os.environ.get('POSTGRES_USER')
-postgres_password=os.environ.get('POSTGRES_PASSWORD')
-postgres_host=os.environ.get('POSTGRES_HOST')
-postgres_port=os.environ.get('POSTGRES_PORT')
 
-output_directory = "../data_collection/sample_output"
+output_directory = "../data_collection/tournament"
+output_directory = os.path.join(os.path.dirname(__file__), output_directory)
 
-def get_connection_string():
-  return f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
+def get_connection():
+    return psycopg. connect(
+        host="localhost",
+        port="5432",
+        dbname="postgres",
+        user="postgres",
+        password=""
+    )
 
 def execute_sql_script(path: str):
-  with psycopg.connect(get_connection_string()) as conn:
-    with conn.cursor() as cur:
-      with open(path) as f:
-        cur.execute(f.read())
+    full_path = os.path.join(os.path.dirname(__file__), path)  # 🔍 construit le chemin absolu
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            with open(full_path) as f:
+                cur.execute(f.read())
+        conn.commit()       
 
 def insert_wrk_tournaments():
-  tournament_data = []
-  for file in os.listdir(output_directory):
-    with open(f"{output_directory}/{file}") as f:
-      tournament = json.load(f)
-      tournament_data.append((
-        tournament['id'], 
-        tournament['name'], 
-        datetime.strptime(tournament['date'], '%Y-%m-%dT%H:%M:%S.000Z'),
-        tournament['organizer'], 
-        tournament['format'], 
-        int(tournament['nb_players'])
-        ))
-  
-  with psycopg.connect(get_connection_string()) as conn:
-    with conn.cursor() as cur:
-      cur.executemany("INSERT INTO public.wrk_tournaments values (%s, %s, %s, %s, %s, %s)", tournament_data)
+    tournament_data = []
+    for file in os.listdir(output_directory):
+        with open(f"{output_directory}/{file}") as f:
+            tournament = json.load(f)
+            tournament_data.append((
+                tournament['id'], 
+                tournament['name'], 
+                datetime.strptime(tournament['date'], '%Y-%m-%dT%H:%M:%S.000Z'),
+                tournament['organizer'], 
+                tournament['format'], 
+                int(tournament['nb_players'])
+            ))
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.executemany(
+                "INSERT INTO public.wrk_tournaments values (%s, %s, %s, %s, %s, %s)",
+                tournament_data
+            )
+        conn.commit()
 
 def insert_wrk_decklists():
-  decklist_data = []
-  for file in os.listdir(output_directory):
-    with open(f"{output_directory}/{file}") as f:
-      tournament = json.load(f)
-      tournament_id = tournament['id']
-      for player in tournament['players']:
-        player_id = player['id']
-        for card in player['decklist']:
-          decklist_data.append((
-            tournament_id,
-            player_id,
-            card['type'],
-            card['name'],
-            card['url'],
-            int(card['count']),
-          ))
-  
-  with psycopg.connect(get_connection_string()) as conn:
-    with conn.cursor() as cur:
-      cur.executemany("INSERT INTO public.wrk_decklists values (%s, %s, %s, %s, %s, %s)", decklist_data)
+    decklist_data = []
+    for file in os.listdir(output_directory):
+        with open(f"{output_directory}/{file}") as f:
+            tournament = json.load(f)
+            tournament_id = tournament['id']
+            for player in tournament['players']:
+                player_id = player['id']
+                for card in player['decklist']:
+                    decklist_data.append((
+                        tournament_id,
+                        player_id,
+                        card['type'],
+                        card['name'],
+                        card['url'],
+                        int(card['count']),
+                    ))
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.executemany(
+                "INSERT INTO public.wrk_decklists values (%s, %s, %s, %s, %s, %s)",
+                decklist_data
+            )
+        conn.commit()
 
 print("creating work tables")
 execute_sql_script("00_create_wrk_tables.sql")
@@ -71,3 +81,5 @@ insert_wrk_decklists()
 
 print("construct card database")
 execute_sql_script("01_dwh_cards.sql")
+
+
